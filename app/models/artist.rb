8deee64e2a0c2a4bf74d2470ff0f3bcabe7693db
geraldarzy <geraldarzola@gmail.com>
2021-02-25 15:17:59 -0500

    commit tree 93655b510138caf5d7cae7d5b298183541157501

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,6 @@
 class Artist < ActiveRecord::Base
-  # add associations here
+  has_many :songs
 end
+#Artist
+#has a name attribute (String)
+#has many Songs
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,6 @@
 class Genre < ActiveRecord::Base
   # add associations
+  has_many :songs
 end
+#has a name attribute (String)
+#has many songs
